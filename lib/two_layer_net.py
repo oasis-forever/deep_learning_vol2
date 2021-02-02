74fd343f2656f37b47cfe0e8286a6d5ca5d4f84d
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-02 15:42:37 +0900

    Commit TwoLayerNet

---
+++ lib/two_layer_net.py
@@ -0,0 +1,24 @@
+import numpy as np
+from affine import Affine
+from sigmoid import Sigmoid
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
---
+++ test/test_two_layer_net.py
@@ -0,0 +1,16 @@
+import unittest
+import numpy as np
+import sys
+sys.path.append("../lib")
+from two_layer_net import TwoLayerNet
+
+class TestTwoLayerNet(unittest.TestCase):
+    def setUp(self):
+        self.two_layer_net = TwoLayerNet(2, 4, 3)
+
+    def test_predict(self):
+        x = np.random.randn(10, 2)
+        self.assertEqual((10, 3), self.two_layer_net.predict(x).shape)
+
+if __name__ == "__main__":
+    unittest.main()
