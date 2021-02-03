09d316f1213e1d8bec6118a1d822f8fa0a85d9b4
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-03 11:10:53 +0900

    Commit MatMul

---
+++ test/test_matmul.py
@@ -0,0 +1,22 @@
+import unittest
+import numpy as np
+import sys
+sys.path.append("../lib")
+from matmul import MatMul
+
+class TestMatMul(unittest.TestCase):
+    def setUp(self):
+        W = np.random.rand(4, 2)
+        self.matmul = MatMul(W)
+
+    def test_forward(self):
+        x = np.random.rand(2, 4)
+        self.assertEqual((2, 2), self.matmul.forward(x).shape)
+
+    def test_backward(self):
+        x = np.random.rand(2, 4)
+        dout = self.matmul.forward(x)
+        self.assertEqual((2, 4), self.matmul.backward(dout).shape)
+
+if __name__ == "__main__":
+    unittest.main()
