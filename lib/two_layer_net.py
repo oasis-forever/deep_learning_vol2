c3e3b84ed66fe4aadd604c03961972ab2f5da16f
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-04 11:54:46 +0900

    Update TwoLayerNet

--- lib/two_layer_net.py
+++ lib/two_layer_net.py
@@ -3,24 +3,44 @@ import sys
 sys.path.append("./concerns")
 from affine import Affine
 from sigmoid import Sigmoid
+from soft_max_with_loss import SoftMaxWithLoss
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size):
-        I, H, O = input_size, hidden_size, output_size
-        W1 = np.random.randn(I, H)
-        b1 = np.random.randn(H)
-        W2 = np.random.randn(H, O)
-        b2 = np.random.randn(O)
+        I  = input_size
+        H  = hidden_size
+        O  = output_size
+        # Initialise heabiness and bias
+        W1 = 0.01 * np.random.randn(I, H)
+        b1 = np.zeros(H)
+        W2 = 0.01 * np.random.randn(H, O)
+        b2 = np.zeros(O)
+        # Generate layer
         self.layers = [
             Affine(W1, b1),
             Sigmoid(),
             Affine(W2, b2)
         ]
+        self.loss_layer = SoftMaxWithLoss()
+        # Integrate all heaviness and gradients in each list
         self.params = []
+        self.grads = []
         for layer in self.layers:
             self.params += layer.params
+            self.grads  += layer.grads
 
-    def predict(self, x):
+    def _predict(self, x):
         for layer in self.layers:
             x = layer.forward(x)
         return x
+
+    def forward(self, x, t):
+        score = self._predict(x)
+        loss  = self.loss_layer.forward(score, t)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
--- test/test_two_layer_net.py
+++ test/test_two_layer_net.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from numpy.testing import assert_almost_equal
 import sys
 sys.path.append("../lib")
 sys.path.append("../lib/concerns")
@@ -8,10 +9,18 @@ from two_layer_net import TwoLayerNet
 class TestTwoLayerNet(unittest.TestCase):
     def setUp(self):
         self.two_layer_net = TwoLayerNet(2, 4, 3)
+        self.x = np.random.randn(4, 2)
+        self.t = np.random.randn(4, 3)
 
     def test_predict(self):
-        x = np.random.randn(10, 2)
-        self.assertEqual((10, 3), self.two_layer_net.predict(x).shape)
+        self.assertEqual((4, 3), self.two_layer_net._predict(self.x).shape)
+
+    def test_forward(self):
+        self.assertEqual(1, int(self.two_layer_net.forward(self.x, self.t)))
+
+    def test_backward(self):
+        self.two_layer_net.forward(self.x, self.t)
+        self.assertEqual((4, 2), self.two_layer_net.backward().shape)
 
 if __name__ == "__main__":
     unittest.main()
