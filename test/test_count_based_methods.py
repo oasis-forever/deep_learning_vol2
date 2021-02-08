e5e323e73944a2c975cda794138458b264c099d3
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-08 16:07:19 +0900

    Share some preprocess in TestCountBasedMethod#setUp

--- test/test_count_based_methods.py
+++ test/test_count_based_methods.py
@@ -11,12 +11,14 @@ class TestCountBasedMethod(unittest.TestCase):
     def setUp(self):
         text = "You said good-bye, and I said hello."
         self.cbm = CountBasedMethod(text)
+        self.cbm.preprocess()
+        vocab_size = len(uniq_list(self.cbm.corpus))
+        self.co_matrix = self.cbm.create_co_matrix(vocab_size)
 
     def test_words(self):
         self.assertEqual(["you", "said", "good-bye", ",", "and", "i", "said", "hello", "."], self.cbm.words)
 
     def test_preprocess(self):
-        self.cbm.preprocess()
         assert_array_equal(np.array([0, 1, 2, 3, 4, 5, 1, 6, 7]), self.cbm.corpus)
         self.assertEqual({
             "you": 0,
@@ -40,8 +42,6 @@ class TestCountBasedMethod(unittest.TestCase):
         }, self.cbm.id_to_word)
 
     def test_create_co_matrix(self):
-        self.cbm.preprocess()
-        vocab_size = len(uniq_list(self.cbm.corpus))
         assert_array_equal(np.array([
             [0, 1, 0, 0, 0, 0, 0, 0],
             [1, 0, 1, 0, 0, 1, 1, 0],
@@ -51,14 +51,11 @@ class TestCountBasedMethod(unittest.TestCase):
             [0, 1, 0, 0, 1, 0, 0, 0],
             [0, 1, 0, 0, 0, 0, 0, 1],
             [0, 0, 0, 0, 0, 0, 1, 0]
-        ]), self.cbm.create_co_matrix(vocab_size))
+        ]), self.co_matrix)
 
     def test_cos_similarity(self):
-        self.cbm.preprocess()
-        vocab_size = len(uniq_list(self.cbm.corpus))
-        co_matrix = self.cbm.create_co_matrix(vocab_size)
-        x = co_matrix[self.cbm.word_to_id["you"]]
-        y = co_matrix[self.cbm.word_to_id["i"]]
+        x = self.co_matrix[self.cbm.word_to_id["you"]]
+        y = self.co_matrix[self.cbm.word_to_id["i"]]
         self.assertEqual(0.7071067691154799, self.cbm.cos_similarity(x, y))
 
 if __name__ == "__main__":
