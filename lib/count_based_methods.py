ef70510a1feabae9a59dc73f34703c0216e7d986
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-09 16:01:27 +0900

    Refactor CountBasedMethod to fix interface of each method

--- lib/count_based_methods.py
+++ lib/count_based_methods.py
@@ -6,19 +6,17 @@ class CountBasedMethod:
         text = text.lower()
         text = text.replace(",", " ,")
         text = text.replace(".", " .")
-        self.words = text.split(" ")
-        self.corpus = None
-        self.word_to_id = None
-        self.id_to_word = None
-        self.co_matrix  = None
-        self.M = None
-        self.U = None
+        self.text = text
 
-    def _take_out_query(self, query):
-        if query not in self.word_to_id:
+    def word_list(self, text):
+        word_list = self.text.split(" ")
+        return word_list
+
+    def _take_out_query(self, query, word_to_id, co_matrix):
+        if query not in word_to_id:
             return "%s is not found." % query
-        query_id  = self.word_to_id[query]
-        query_vec = self.co_matrix[query_id]
+        query_id  = word_to_id[query]
+        query_vec = co_matrix[query_id]
         return {"query": query}, query_vec
 
     def _cos_similarity(self, x, y, eps=1e-8):
@@ -27,76 +25,79 @@ class CountBasedMethod:
         cos_similarity = np.dot(nx, ny)
         return cos_similarity
 
-    def _calc_cos_similarity(self, query_vec):
-        vocab_size = len(self.id_to_word)
+    def _calc_cos_similarity(self, vocab_size, co_matrix, query_vec):
         similarity = np.zeros(vocab_size)
         for i in range(vocab_size):
-            similarity[i] = self._cos_similarity(self.co_matrix[i], query_vec)
+            similarity[i] = self._cos_similarity(co_matrix[i], query_vec)
         return similarity
 
-    def _output_result_asc(self, similarity, query, top=5):
+    def _output_result_asc(self, similarity, query, id_to_word, top=5):
         count = 0
         result = {}
         for i in (-1 * similarity).argsort():
-            if self.id_to_word[i] == query:
+            if id_to_word[i] == query:
                 continue
-            result[self.id_to_word[i]] = similarity[i]
+            result[id_to_word[i]] = similarity[i]
             count += 1
             if count >= top:
                 return result
 
-    def preprocess(self):
-        self.word_to_id = {}
-        self.id_to_word = {}
-        for word in self.words:
-            if word not in self.word_to_id:
-                new_id = len(self.word_to_id)
-                self.word_to_id[word] = new_id
-                self.id_to_word[new_id] = word
-        self.corpus = np.array([self.word_to_id[w] for w in self.words])
+    def preprocess(self, word_list):
+        word_to_id = {}
+        id_to_word = {}
+        for word in word_list:
+            if word not in word_to_id:
+                new_id = len(word_to_id)
+                word_to_id[word] = new_id
+                id_to_word[new_id] = word
+        corpus = np.array([word_to_id[w] for w in word_list])
+        return word_to_id, id_to_word, corpus
 
-    def create_co_matrix(self, vocab_size, windows_size=1):
-        corpus_size = len(self.corpus)
-        self.co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
-        for index, word_id in enumerate(self.corpus):
+    def create_co_matrix(self, corpus, vocab_size, windows_size=1):
+        corpus_size = len(corpus)
+        co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+        for index, word_id in enumerate(corpus):
             for i in range(1, windows_size + 1):
                 left_index  = index - i
                 right_index = index + i
                 if left_index >= 0:
-                    left_word_id = self.corpus[left_index]
-                    self.co_matrix[word_id, left_word_id] += 1
+                    left_word_id = corpus[left_index]
+                    co_matrix[word_id, left_word_id] += 1
                 if right_index < corpus_size:
-                    right_word_id = self.corpus[right_index]
-                    self.co_matrix[word_id, right_word_id] += 1
+                    right_word_id = corpus[right_index]
+                    co_matrix[word_id, right_word_id] += 1
+        return co_matrix
 
-    def rank_similarities(self, query, top=5):
-        query_info, query_vec = self._take_out_query(query)
-        similarity = self._calc_cos_similarity(query_vec)
-        result = self._output_result_asc(similarity, query, top)
+    def rank_similarities(self, query, word_to_id, co_matrix, vocab_size, id_to_word, top=5):
+        query_info, query_vec = self._take_out_query(query, word_to_id, co_matrix)
+        similarity = self._calc_cos_similarity(vocab_size, co_matrix, query_vec)
+        result = self._output_result_asc(similarity, query, id_to_word)
         query_info.update(result)
         return query_info
 
     # Positive Pointwise Mutual Information
-    def ppmi(self, verbose=False, eps=1e-8):
-        self.M = np.zeros_like(self.co_matrix, dtype=np.float32)
-        N = np.sum(self.co_matrix)
-        S = np.sum(self.co_matrix, axis=0)
-        total = self.co_matrix.shape[0] * self.co_matrix.shape[1]
+    def ppmi(self, co_matrix, verbose=False, eps=1e-8):
+        M = np.zeros_like(co_matrix, dtype=np.float32)
+        N = np.sum(co_matrix)
+        S = np.sum(co_matrix, axis=0)
+        total = co_matrix.shape[0] * co_matrix.shape[1]
         count = 0
-        for i in range(self.co_matrix.shape[0]):
-            for j in range(self.co_matrix.shape[1]):
-                pmi = np.log2(self.co_matrix[i, j] * N / (S[j] * S[i]) + eps)
-                self.M[i, j] = max(0, pmi)
+        for i in range(co_matrix.shape[0]):
+            for j in range(co_matrix.shape[1]):
+                pmi = np.log2(co_matrix[i, j] * N / (S[j] * S[i]) + eps)
+                M[i, j] = max(0, pmi)
                 if verbose:
                     count += 1
                     if count % (total // 100) == 0:
                         print("%.1f%% done" % (100 * count / total))
+        return M
 
-    def singular_value_deconposition(self):
-        self.U, S, V = np.linalg.svd(self.M)
+    def singular_value_deconposition(self, M):
+        U, S, V = np.linalg.svd(M)
+        return U
 
-    def save_svd_plot_image(self, path):
-        for word, word_id in self.word_to_id.items():
-            plt.annotate(word, (self.U[word_id, 0], self.U[word_id, 1]))
-        plt.scatter(self.U[:, 0], self.U[:, 1], alpha=0.5)
+    def save_svd_plot_image(self, word_to_id, U, path):
+        for word, word_id in word_to_id.items():
+            plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+        plt.scatter(U[:, 0], U[:, 1], alpha=0.5)
         plt.savefig(path)
--- test/test_count_based_methods.py
+++ test/test_count_based_methods.py
@@ -11,43 +11,63 @@ class TestCountBasedMethod(unittest.TestCase):
     def setUp(self):
         text = "You said good-bye and I said hello."
         self.cbm = CountBasedMethod(text)
-        self.cbm.preprocess()
-        vocab_size = len(self.cbm.word_to_id)
-        self.cbm.create_co_matrix(vocab_size)
-        self.query = "you"
 
     def test_words(self):
-        self.assertEqual(["you", "said", "good-bye", "and", "i", "said", "hello", "."], self.cbm.words)
+        word_list = self.cbm.word_list(self.cbm.text)
+        self.assertEqual(["you", "said", "good-bye", "and", "i", "said", "hello", "."], word_list)
 
     def test_take_out_query(self):
-        query_info, query_vec = self.cbm._take_out_query(self.query)
+        query = "you"
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        query_info, query_vec = self.cbm._take_out_query(query, word_to_id, co_matrix)
         self.assertEqual({"query": "you"}, query_info)
         assert_array_equal(np.array([0, 1, 0, 0, 0, 0, 0]), query_vec)
 
     def test_cos_similarity(self):
-        x = self.cbm.co_matrix[self.cbm.word_to_id["you"]]
-        y = self.cbm.co_matrix[self.cbm.word_to_id["i"]]
-        self.assertEqual(0.7071067691154799, self.cbm._cos_similarity(x, y))
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        x = co_matrix[word_to_id["you"]]
+        y = co_matrix[word_to_id["i"]]
+        cos_similarity = self.cbm._cos_similarity(x, y)
+        self.assertEqual(0.7071067691154799, cos_similarity)
 
     def test_calc_cos_similarity(self):
-        *_, query_vec = self.cbm._take_out_query(self.query)
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        query = "you"
+        *_, query_vec = self.cbm._take_out_query(query, word_to_id, co_matrix)
+        similarity = self.cbm._calc_cos_similarity(vocab_size, co_matrix, query_vec)
         assert_almost_equal(np.array([
             1., 0., 0.7071068, 0., 0.7071068, 0.7071068, 0.
-        ]), self.cbm._calc_cos_similarity(query_vec))
+        ]), similarity)
 
     def test_output_result_asc(self):
-        *_, query_vec = self.cbm._take_out_query(self.query)
-        similarity = self.cbm._calc_cos_similarity(query_vec)
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, id_to_word, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        query = "you"
+        *_, query_vec = self.cbm._take_out_query(query, word_to_id, co_matrix)
+        similarity = self.cbm._calc_cos_similarity(vocab_size, co_matrix, query_vec)
+        result = self.cbm._output_result_asc(similarity, query, id_to_word)
         self.assertEqual({
             "good-bye": 0.7071067691154799,
             "i": 0.7071067691154799,
             "hello": 0.7071067691154799,
             "said": 0.0,
             "and": 0.0
-        }, self.cbm._output_result_asc(similarity, self.query))
+        }, result)
 
     def test_preprocess(self):
-        assert_array_equal(np.array([0, 1, 2, 3, 4, 1, 5, 6]), self.cbm.corpus)
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, id_to_word, corpus = self.cbm.preprocess(word_list)
         self.assertEqual({
             "you": 0,
             "said": 1,
@@ -56,7 +76,7 @@ class TestCountBasedMethod(unittest.TestCase):
             "i": 4,
             "hello": 5,
             ".": 6
-        }, self.cbm.word_to_id)
+        }, word_to_id)
         self.assertEqual({
             0: "you",
             1: "said",
@@ -65,9 +85,14 @@ class TestCountBasedMethod(unittest.TestCase):
             4: "i",
             5: "hello",
             6: "."
-        }, self.cbm.id_to_word)
+        }, id_to_word)
+        assert_array_equal(np.array([0, 1, 2, 3, 4, 1, 5, 6]), corpus)
 
     def test_create_co_matrix(self):
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
         assert_array_equal(np.array([
             [0, 1, 0, 0, 0, 0, 0],
             [1, 0, 1, 0, 1, 1, 0],
@@ -76,9 +101,15 @@ class TestCountBasedMethod(unittest.TestCase):
             [0, 1, 0, 1, 0, 0, 0],
             [0, 1, 0, 0, 0, 0, 1],
             [0, 0, 0, 0, 0, 1, 0]
-        ]), self.cbm.co_matrix)
+        ]), co_matrix)
 
     def test_rank_similarity(self):
+        query = "you"
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, id_to_word, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        top_five_similarities = self.cbm.rank_similarities(query, word_to_id, co_matrix, vocab_size, id_to_word)
         self.assertEqual({
             'query': 'you',
             'good-bye': 0.7071067691154799,
@@ -86,10 +117,14 @@ class TestCountBasedMethod(unittest.TestCase):
             'hello': 0.7071067691154799,
             'said': 0.0,
             'and': 0.0
-        }, self.cbm.rank_similarities(self.query))
+        }, top_five_similarities)
 
     def test_ppmi(self):
-        self.cbm.ppmi()
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        M = self.cbm.ppmi(co_matrix)
         assert_almost_equal(np.array([
             [0., 1.8073549, 0., 0., 0., 0., 0.],
             [1.8073549, 0., 0.8073549, 0., 0.8073549, 0.8073549, 0.],
@@ -98,11 +133,15 @@ class TestCountBasedMethod(unittest.TestCase):
             [0., 0.8073549, 0., 1.8073549, 0., 0., 0.],
             [0., 0.8073549, 0., 0., 0., 0., 2.807355],
             [0., 0., 0., 0., 0., 2.807355, 0.]
-        ]), self.cbm.M)
+        ]), M)
 
     def test_svd(self):
-        self.cbm.ppmi()
-        self.cbm.singular_value_deconposition()
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        M = self.cbm.ppmi(co_matrix)
+        U = self.cbm.singular_value_deconposition(M)
         assert_almost_equal(np.array([
             [ 3.40948761e-01,  0.00000000e+00, -1.20516241e-01, -3.88578059e-16, -9.32324946e-01, -1.11022302e-16, -2.42574685e-17],
             [ 0.00000000e+00, -5.97636402e-01,  0.00000000e+00,  1.80237904e-01,  0.00000000e+00, -7.81245828e-01,  0.00000000e+00],
@@ -111,12 +150,16 @@ class TestCountBasedMethod(unittest.TestCase):
             [ 4.36312199e-01, -3.12375064e-17, -5.08782864e-01, -1.59998290e-16,  2.25325629e-01, -1.30164976e-17,  7.07106769e-01],
             [ 7.09237099e-01, -3.12375064e-17,  6.83926761e-01, -1.59998290e-16,  1.70958877e-01, -1.30164976e-17,  2.31390806e-17],
             [-1.66533454e-16, -6.28488600e-01, -4.16333634e-17, -7.10334539e-01,  2.22044605e-16,  3.16902101e-01, -9.61431563e-17]
-        ]), self.cbm.U)
+        ]), U)
 
     def test_save_svd_plot_image(self):
-        self.cbm.ppmi()
-        self.cbm.singular_value_deconposition()
-        self.cbm.save_svd_plot_image("../img/svd_plot.png")
+        word_list = self.cbm.word_list(self.cbm.text)
+        word_to_id, _, corpus = self.cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        co_matrix = self.cbm.create_co_matrix(corpus, vocab_size)
+        M = self.cbm.ppmi(co_matrix)
+        U = self.cbm.singular_value_deconposition(M)
+        self.cbm.save_svd_plot_image(word_to_id, U, "../img/svd_plot.png")
         self.assertEqual(True, path.exists("../img/svd_plot.png"))
 
 if __name__ == "__main__":
