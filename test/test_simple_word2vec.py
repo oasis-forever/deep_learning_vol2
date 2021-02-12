acc710bb54fe693b10aaf2fb9f7054f66511e0ec
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-12 23:41:28 +0900

    Update TestSimpleWord2Vec

--- test/test_simple_word2vec.py
+++ test/test_simple_word2vec.py
@@ -13,12 +13,14 @@ class TestSimpleWord2Vec(unittest.TestCase):
         word_list = cbm.text_to_word_list(text)
         self.word_to_id, _, self.corpus = cbm.preprocess(word_list)
         self.simple_word2vec = SimpleWord2Vec()
+        self.contexts_array, self.target_array = self.simple_word2vec.create_contexts_target(self.corpus)
+        self.vocab_size = len(self.word_to_id)
 
     def test_corpus(self):
         assert_array_equal(np.array([0, 1, 2, 3, 4, 1, 5, 6]), self.corpus)
 
     def test_create_contexts_target(self):
-        contexts_array, target_array = self.simple_word2vec.create_contexts_target(self.corpus)
+        self.assertEqual((6, 2), self.contexts_array.shape)
         assert_array_equal(np.array([
             [0, 2],
             [1, 3],
@@ -26,22 +28,49 @@ class TestSimpleWord2Vec(unittest.TestCase):
             [3, 1],
             [4, 5],
             [1, 6]
-        ]), contexts_array)
-        assert_array_equal(np.array([1, 2, 3, 4, 1, 5]), target_array)
+        ]), self.contexts_array)
+        self.assertEqual((6,), self.target_array.shape)
+        assert_array_equal(np.array([1, 2, 3, 4, 1, 5]), self.target_array)
 
     def test_convert_to_one_hot(self):
-        vocab_size = len(self.word_to_id)
-        one_hot = self.simple_word2vec.convert_to_one_hot(self.corpus, vocab_size)
+        contexts = self.simple_word2vec.convert_to_one_hot(self.contexts_array, self.vocab_size)
+        self.assertEqual((6, 2, 7), contexts.shape)
         assert_array_equal(np.array([
-            [1, 0, 0, 0, 0, 0, 0],
+            [
+                [1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0]
+            ],
+            [
+                [0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0]
+            ],
+            [
+                [0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0]
+            ],
+            [
+                [0, 0, 0, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0]
+            ],
+            [
+                [0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1]
+            ]
+        ]), contexts)
+        target = self.simple_word2vec.convert_to_one_hot(self.target_array, self.vocab_size)
+        self.assertEqual((6, 7), target.shape)
+        assert_array_equal(np.array([
             [0, 1, 0, 0, 0, 0, 0],
             [0, 0, 1, 0, 0, 0, 0],
             [0, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0],
             [0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 1]
-        ]), one_hot)
+            [0, 0, 0, 0, 0, 1, 0]
+        ]), target)
 
 if __name__ == "__main__":
     unittest.main()
