04cdb9a08d8319ac2ef7d8733d4767186298edf6
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-20 12:46:29 +0900

    Update UnigramSampler and TestUnigramSampler

--- lib/concerns/unigram_sampler.py
+++ lib/concerns/unigram_sampler.py
@@ -1,17 +1,15 @@
 import numpy as np
+import collections
 
 class UnigramSampler:
     def __init__(self, corpus, power, sample_size):
         self.sample_size = sample_size
-        self.vocab_size  = None
-        self.word_p      = None
         counts = collections.Counter()
         for word_id in corpus:
             counts[word_id] += 1
-        vocab_size = len(counts)
-        self.vocab_size = vocab_size
-        self.word_p = np.zeros(vocab_size)
-        for i in range(vocab_size):
+        self.vocab_size = len(counts)
+        self.word_p = np.zeros(self.vocab_size)
+        for i in range(self.vocab_size):
             self.word_p[i] = counts[i]
         self.word_p = np.power(self.word_p, power)
         self.word_p /= np.sum(self.word_p)
@@ -21,8 +19,8 @@ class UnigramSampler:
         negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
         for i in range(batch_size):
             p = self.word_p.copy()
-            target_idx = target[i]
-            p[target_idx] = 0
+            target_index = target[i]
+            p[target_index] = 0
             p /= p.sum()
             negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
         return negative_sample
--- test/test_unigram_sampler.py
+++ test/test_unigram_sampler.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_almost_equal
 import sys
 sys.path.append("../lib/concerns")
 from unigram_sampler import UnigramSampler
@@ -9,15 +9,19 @@ class TestUnigramSampler(unittest.TestCase):
     def setUp(self):
         corpus      = np.array([0, 1, 2, 3, 4, 1, 2, 3])
         power       = 0.75
-        sample_size = 5
+        sample_size = 2
         self.unigram_sampler = UnigramSampler(corpus, power, sample_size)
 
+    def test_word_p(self):
+        word_p = self.unigram_sampler.word_p
+        assert_almost_equal(np.array(
+            [0.141937, 0.2387087, 0.2387087, 0.2387087, 0.141937]
+        ), word_p)
+
     def test_get_negative_sample(self):
-        target = None
+        target = np.array([1, 3, 0])
         negative_sample = self.unigram_sampler.get_negative_sample(target)
-        assert_array_equal(np.array([
-            None
-       ]), negative_sample)
+        self.assertEqual((3, 2), negative_sample.shape)
 
 if __name__ == "__main__":
     unittest.main()
