f869933bee192a5534ecaf0f6a44f3ae76a9cf42
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-20 23:07:29 +0900

    Update NegativeSamplingLoss

--- lib/layers/negative_sampling_loss.py
+++ lib/layers/negative_sampling_loss.py
@@ -6,35 +6,35 @@ from sigmoid_with_loss import SigmoidWithLoss
 from embedding_dot import EmbeddingDot
 
 class NegativeSamplingLoss:
-    def __init__(self, W, corpus, power=0.75, sample_size=5):
-        self.sample_size      = sample_size
-        self.sampler          = UnigramSampler(corpus, power, sample_size)
-        self.loss_layers      = [SigmoidWithLoss() for _ in range(sample_size + 1)]
-        self.embedding_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+    def __init__(self, W, corpus, sample_size=5, power=0.75):
+        self.sample_size          = sample_size
+        self.sampler              = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers          = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embedding_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
         self.params = []
         self.grads  = []
-        for embedding_layer in self.embedding_layers:
-            self.params += embedding_layer.params
-            self.grads  += embedding_layer.grads
+        for embedding_dot_layer in self.embedding_dot_layers:
+            self.params += embedding_dot_layer.params
+            self.grads  += embedding_dot_layer.grads
 
     def forward(self, h, target):
-        batch_size = target,shape[0]
+        batch_size = target.shape[0]
         neative_sample = self.sampler.get_negative_sample(target)
         # Correct sample
-        score = self.embedding_layers[0].forward(h, target)
+        score = self.embedding_dot_layers[0].forward(h, target)
         correct_label = np.ones(batch_size, dtype=np.int32)
         loss = self.loss_layers[0].forward(score, correct_label)
         # Negative sample
         negative_label = np.zeros(batch_size, dtype=np.int32)
         for i in range(self.sample_size):
             negative_target = neative_sample[:, i]
-            score = self.embedding_layers[1 + i].forward(h, negative_target)
+            score = self.embedding_dot_layers[1 + i].forward(h, negative_target)
             loss += self.loss_layers[1 + i].forward(score, negative_label)
         return loss
 
     def backward(self, dout=1):
         dh = 0
-        for layer0, layer1 in zip(self.loss_layers, self.embedding_layers):
-            dscore = layer0.backward(dout)
-            dh += layer1(dscore)
+        for loss_layer, embedding_dot_layer in zip(self.loss_layers, self.embedding_dot_layers):
+            dscore = loss_layer.backward(dout)
+            dh += embedding_dot_layer.backward(dscore)
         return dh
--- test/test_negative_sampling_loss.py
+++ test/test_negative_sampling_loss.py
@@ -9,41 +9,47 @@ from negative_sampling_loss import NegativeSamplingLoss
 class TestNegativeSamplingLoss(unittest.TestCase):
     def setUp(self):
         W = np.arange(21).reshape(7, 3)
-        corpus = np.array([0, 1, 2, 3, 4, 1, 2, 3])
+        corpus = np.array([0, 1, 2, 3, 4, 1, 5, 2, 6])
         self.negative_sampling_loss = NegativeSamplingLoss(W, corpus)
-
-    def test_loss_layers(self):
-        loss_layers = self.negative_sampling_loss.loss_layers
-        self.assertEqual([], loss_layers)
-
-    def test_embedding_layers(self):
-        embedding_layers = self.negative_sampling_loss.embedding_layers
-        self.assertEqual([], embedding_layers)
+        self.h = np.arange(3)
+        self.target = np.array([1, 3, 0])
 
     def test_params(self):
-        params = self.negative_sampling_loss.params
+        param, *_ = self.negative_sampling_loss.params
         assert_array_equal(np.array([
-            None
-        ]), params)
+            [ 0,  1,  2],
+            [ 3,  4,  5],
+            [ 6,  7,  8],
+            [ 9, 10, 11],
+            [12, 13, 14],
+            [15, 16, 17],
+            [18, 19, 20]
+        ]), param)
 
     def test_grads(self):
-        grads = self.negative_sampling_loss.grads
+        grad, *_ = self.negative_sampling_loss.grads
         assert_array_equal(np.array([
-            None
-        ]), grads)
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]), grad)
 
     def test_forward(self):
-        h = None
-        target = None
-        loss = self.negative_sampling_loss.forward(h, target)
-        self.assertEqual(None, loss)
+        loss = self.negative_sampling_loss.forward(self.h, self.target)
+        self.assertEqual(True, 65 <= loss < 75)
 
     def test_backward(self):
-        h = None
-        target = None
-        self.negative_sampling_loss.forward(h, target)
+        self.negative_sampling_loss.forward(self.h, self.target)
         dh = self.negative_sampling_loss.backward()
-        self.assertEqual(None, dh)
+        assert_array_equal(np.array([
+            [0. ,  1.61,  3.22],
+            [5. ,  6.67,  8.33],
+            [10., 11.67, 13.33]
+        ]), np.round(dh, decimals=2))
 
 if __name__ == "__main__":
     unittest.main()
