76b3c1c864f8ad5f189bc2db75c9ca31b2dea267
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-22 01:01:07 +0900

    Commit CBOW

---
+++ lib/cbow.py
@@ -0,0 +1,43 @@
+import numpy as np
+import sys
+sys.path.append("./layers")
+from embedding import Embedding
+from negative_sampling_loss import NegativeSamplingLoss
+
+class CBOW:
+    def __init__(self, vocab_size, hidden_size, window_size, corpus):
+        V = vocab_size
+        H = hidden_size
+        # Initialise weight
+        W_in  = 0.01 * np.random.randn(V, H).astype("f")
+        W_out = 0.01 * np.random.randn(V, H).astype("f")
+        # Generate layers
+        self.in_layers = []
+        for i in range(2 * window_size):
+            in_layer = Embedding(W_in)
+            self.in_layers.append(in_layer)
+        self.ns_loss_layer = NegativeSamplingLoss(W_out, corpus)
+        # Integrate all weight and gradients in a list
+        layers = self.in_layers + [self.ns_loss_layer]
+        self.params = []
+        self.grads  = []
+        for layer in layers:
+            self.params += layer.params
+            self.grads  += layer.grads
+        # Assign a word embedding to an instance variable
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h = 0
+        for i, in_layer in enumerate(self.in_layers):
+            h += in_layer.forward(contexts[:, i])
+        h *= 1 / len(self.in_layers)
+        loss = self.ns_loss_layer.forward(h, target)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.ns_loss_layer.backward(dout)
+        dout *= 1 / len(self.in_layers)
+        for layer in self.in_layers:
+            layer.backward(dout)
+        return None
---
+++ test/test_cbow.py
@@ -0,0 +1,95 @@
+import unittest
+import numpy as np
+from numpy.testing import assert_array_equal
+import copy
+import sys
+sys.path.append("../lib")
+sys.path.append("../lib/concerns")
+sys.path.append("../lib/layers")
+from cbow import CBOW
+from simple_word2vec import SimpleWord2Vec
+from count_based_methods import CountBasedMethod
+
+class TestSimpleCBOW(unittest.TestCase):
+    def setUp(self):
+        text = "You said good-bye and I said hello."
+        cbm = CountBasedMethod()
+        word_list = cbm.text_to_word_list(text)
+        word_to_id, _, self.corpus = cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        hidden_size = 2
+        window_size = 1
+        self.cbow = CBOW(vocab_size, hidden_size, window_size, self.corpus)
+        self.simple_word2vec = SimpleWord2Vec()
+        self.contexts, self.target = self.simple_word2vec.create_contexts_target(self.corpus)
+
+    def test_forward(self):
+        loss = self.cbow.forward(self.contexts, self.target)
+        self.assertEqual(4.159, round(loss, 3))
+
+    def test_params_diff(self):
+        in_layer, *_ = self.cbow.in_layers
+        before_in_layer_param, = in_layer.params
+        before_in_layer_param = copy.copy(before_in_layer_param)
+        before_ns_loss_layer_param, *_ = self.cbow.ns_loss_layer.params
+        before_ns_loss_layer_param = copy.copy(before_ns_loss_layer_param)
+        self.cbow.forward(self.contexts, self.target)
+        self.cbow.backward()
+        in_layer, *_ = self.cbow.in_layers
+        after_in_layer_param, = in_layer.params
+        after_ns_loss_layer_param, *_ = self.cbow.ns_loss_layer.params
+        in_layer_param = before_in_layer_param == after_in_layer_param
+        ns_loss_layer_param = before_ns_loss_layer_param == after_ns_loss_layer_param
+        assert_array_equal(np.array([
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True]
+        ]), in_layer_param)
+        assert_array_equal(np.array([
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True],
+            [ True,  True]
+        ]), ns_loss_layer_param)
+
+    def test_grads_diff(self):
+        in_layer, *_ = self.cbow.in_layers
+        before_in_layer_grad, = in_layer.grads
+        before_in_layer_grad = copy.copy(before_in_layer_grad)
+        before_ns_loss_layer_grad, *_ = self.cbow.ns_loss_layer.grads
+        before_ns_loss_layer_grad = copy.copy(before_ns_loss_layer_grad)
+        self.cbow.forward(self.contexts, self.target)
+        self.cbow.backward()
+        in_layer, *_ = self.cbow.in_layers
+        after_in_layer_grad, = in_layer.grads
+        after_ns_loss_layer_grad, *_ = self.cbow.ns_loss_layer.grads
+        in_layer_grad = before_in_layer_grad == after_in_layer_grad
+        ns_loss_layer_grad = before_ns_loss_layer_grad == after_ns_loss_layer_grad
+        assert_array_equal(np.array([
+            [False, False],
+            [False, False],
+            [False, False],
+            [False, False],
+            [False, False],
+            [ True,  True],
+            [ True,  True]
+        ]), in_layer_grad)
+        assert_array_equal(np.array([
+            [ True,  True],
+            [False, False],
+            [False, False],
+            [False, False],
+            [False, False],
+            [False, False],
+            [ True,  True]
+        ]), ns_loss_layer_grad)
+
+if __name__ == "__main__":
+    unittest.main()
