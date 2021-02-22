59a98e792fe4656770b9568cff6cac07ffb9c898
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-22 17:08:13 +0900

    Commit train3 exec file

---
+++ exec/train3.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pickle
+import sys
+sys.path.append("../lib")
+sys.path.append("../lib/concerns")
+sys.path.append("../lib/layers")
+sys.path.append("../lib/models")
+sys.path.append("../lib/optimisers")
+from simple_word2vec import SimpleWord2Vec
+from trainer import Trainer
+from cbow import CBOW
+from adam import Adam
+from ptb import *
+
+# hyoer params
+window_size = 5
+hidden_size = 100
+batch_size  = 100
+max_epoch   = 10
+
+# Load data
+corpus, word_to_id, id_to_word = load_data("train")
+vocab_size = len(word_to_id)
+simple_word2vec = SimpleWord2Vec()
+contexts, target = simple_word2vec.create_contexts_target(corpus, window_size)
+
+# Generate a model, trainer and trainer
+model = CBOW(vocab_size, hidden_size, window_size, corpus)
+optimiser = Adam()
+trainer = Trainer(model, optimiser)
+
+# Initialize training
+loss_list = trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.save_plot_image(loss_list, "../img/cbow.png")
+
+# Store data to later use
+word_vecs = model.word_vecs
+
+params = {}
+params["word_vecs"]  = word_vecs.astype(np.float16)
+params["word_to_id"] = word_to_id
+params["id_to_word"] = id_to_word
+pkl_file = "./cbow_params.pkl"
+with open(pkl_file, "wb") as f:
+    pickle.dump(params, f, -1)
