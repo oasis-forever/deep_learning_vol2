35353801f7cd8b92f7e559c75292fa156c1817f0
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-23 20:16:59 +0900

    Commit TimeRNN

---
+++ test/test_time_rnn.py
@@ -0,0 +1,33 @@
+import unittest
+import numpy as np
+from numpy.testing import assert_array_equal
+import sys
+sys.path.append("../lib/layers")
+from time_rnn import TimeRNN
+
+class TestTimeRNN(unittest.TestCase):
+    def setUp(self):
+        Wx = np.random.randn(3, 3)
+        Wh = np.random.randn(3, 3)
+        b  = np.random.randn(3,)
+        self.time_rnn = TimeRNN(Wx, Wh, b)
+        self.xs = np.random.randn(3, 3, 3)
+
+    def test_state(self):
+        h = np.random.randn(7, 7)
+        self.time_rnn.set_state(h)
+        assert_array_equal(h, self.time_rnn.h)
+        self.time_rnn.reset_state()
+        self.assertEqual(None, self.time_rnn.h)
+
+    def test_forward(self):
+        hs = self.time_rnn.forward(self.xs)
+        self.assertEqual((3, 3, 3), hs.shape)
+
+    def test_backward(self):
+        hs = self.time_rnn.forward(self.xs)
+        dh = self.time_rnn.backward(hs)
+        self.assertEqual((3, 3), dh.shape)
+
+if __name__ == "__main__":
+    unittest.main()
