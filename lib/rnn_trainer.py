aed4ae5f71e31a9ce5dfdf48f91087a973a94588
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-25 03:02:55 +0900

    Commit RnnlmTrainer

---
+++ lib/rnn_trainer.py
@@ -0,0 +1,99 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from clip_grads import *
+
+class RnnlmTrainer:
+    def __init__(self, model, optimizer):
+        self.model         = model
+        self.optimizer     = optimizer
+        self.time_index    = 0
+        self.ppl_list      = []
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def _remove_duplicate(self, params, grads):
+        """
+        Integrate duplicate weights into one and add gradients corresponding to the weights.
+        """
+        # Copy list
+        params = self.model.params[:]
+        grads  = self.model.grads[:]
+        while True:
+            find_flg = False
+            L = len(params)
+            for i in range(0, L - 1):
+                for j in range(i + 1, L):
+                    # In case of sharing heaviness
+                    if params[i] is params[j]:
+                        # Add gradient
+                        grads[i] += grads[j]
+                        find_flg = True
+                        params.pop(j)
+                        grads.pop(j)
+                    # In case of sharing heaviness as transpose list(weight typing)
+                    elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                         params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                        grads[i] += grads[j].T
+                        find_flg = True
+                        params.pop(j)
+                        grads.pop(j)
+                    if find_flg: break
+                if find_flg: break
+            if not find_flg: break
+        return params, grads
+
+    def _get_batch(self, x, t, batch_size, time_size):
+        batch_x = np.empty((batch_size, time_size), dtype="i")
+        batch_t = np.empty((batch_size, time_size), dtype="i")
+        data_size = len(x)
+        jump = data_size // batch_size
+        # Initial position to load each sample of mini batches
+        offsets = [i * jump for i in range(batch_size)]
+        for time in range(time_size):
+            for i, offset in enumerate(offsets):
+                batch_x[i, time] = x[(offset + self.time_index) % data_size]
+                batch_t[i, time] = t[(offset + self.time_index) % data_size]
+            self.time_index += 1
+        return batch_x, batch_t
+
+    def fit(self, xs, ts, max_epoch=10, batch_size=20, time_size=35, max_grad=None, eval_interval=20):
+        data_size = len(xs)
+        max_iters = data_size // (batch_size * time_size)
+        self.eval_interval = eval_interval
+        total_loss = 0
+        loss_count = 0
+        start_time = time.time()
+        training_process = []
+        for epoch in range(max_epoch):
+            for iters in range(max_iters):
+                batch_x, batch_t = self._get_batch(xs, ts, batch_size, time_size)
+                # Calculate gradients and update parameters
+                loss = self.model.forward(batch_x, batch_t)
+                self.model.backward()
+                params, grads = self._remove_duplicate(self.model.params, self.model.grads)
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                self.optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+                # Eveluate perplexity
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    ppl = np.exp(total_loss / loss_count)
+                    elapsed_time = time.time() - start_time
+                    training_process.append("| Epoch %d |  Iter %d / %d | Time %d[s] | Perplexity %.2f" % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, ppl))
+                    self.ppl_list.append(float(ppl))
+                    total_loss = 0
+                    loss_count = 0
+            self.current_epoch += 1
+        return training_process
+
+    def save_plot_image(self, path, ylim=None):
+        plt.figure()
+        x = np.arange(len(self.ppl_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.ppl_list, label="train")
+        plt.xlabel("Iterations (x{})".format(str(self.eval_interval)))
+        plt.ylabel("Perplexity")
+        plt.savefig(path)
---
+++ test/test_rnn_trainer.py
@@ -0,0 +1,70 @@
+import unittest
+import numpy as np
+from numpy.testing import assert_array_equal
+import os.path
+from os import path
+import sys
+sys.path.append("../lib")
+sys.path.append("../lib/concerns")
+sys.path.append("../lib/optimisers")
+from rnn_trainer import RnnlmTrainer
+from simple_rnnlm import SimpleRNNLM
+from ptb import *
+from sgd import SGD
+
+class TestRnnlmTrainer(unittest.TestCase):
+    def setUp(self):
+        self.batch_size = 10
+        wordvec_size    = 100
+        hidden_size     = 100
+        self.time_size  = 5
+        learning_rate   = 0.1
+        self.max_epoch  = 100
+        corpus, word_to_id, id_to_word = load_data("train")
+        corpus_size = 1000
+        corpus = corpus[:corpus_size]
+        vocab_size = int(max(corpus) + 1)
+        self.xs = corpus[:-1]
+        self.ts = corpus[1:]
+        model = SimpleRNNLM(vocab_size, wordvec_size, hidden_size)
+        optimiser = SGD(learning_rate)
+        self.rnn_trainer = RnnlmTrainer(model, optimiser)
+
+    def test_get_batch(self):
+        batch_x, batch_t = self.rnn_trainer._get_batch(self.xs, self.ts, self.batch_size, self.time_size)
+        assert_array_equal(np.array([
+            [  0,   1,   2,   3,   4],
+            [ 42,  76,  77,  64,  78],
+            [ 26,  26,  98,  56,  40],
+            [ 24,  32,  26, 175,  98],
+            [208, 209,  80, 197,  32],
+            [ 26,  79,  26,  80,  32],
+            [274, 275, 276,  42,  61],
+            [ 88, 303,  26, 304,  26],
+            [ 42,  35,  72, 350,  64],
+            [339, 359, 181, 328, 386]
+        ]), batch_x)
+        assert_array_equal(np.array([
+            [  1,   2,   3,   4,   5],
+            [ 76,  77,  64,  78,  79],
+            [ 26,  98,  56,  40, 128],
+            [ 32,  26, 175,  98,  61],
+            [209,  80, 197,  32,  82],
+            [ 79,  26,  80,  32, 241],
+            [275, 276,  42,  61,  24],
+            [303,  26, 304,  26,  32],
+            [ 35,  72, 350,  64,  27],
+            [359, 181, 328, 386, 387]
+        ]), batch_t)
+
+    def test_fit(self):
+        training_process = self.rnn_trainer.fit(self.xs, self.ts, self.max_epoch, self.batch_size, self.time_size)
+        self.assertEqual(100, len(training_process))
+
+    def test_save_plot_image(self):
+        self.rnn_trainer.fit(self.xs, self.ts, self.max_epoch, self.batch_size, self.time_size)
+        self.rnn_trainer.save_plot_image("../img/rnnlm_trainer.png")
+        self.assertEqual(True, path.exists("../img/rnnlm_trainer.png"))
+
+if __name__ == "__main__":
+    unittest.main()
