5b29e99e6e6b4a39c223fd43bcae79f9487ed018
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-24 23:59:56 +0900

    Commit SimpleRNNLM

---
+++ lib/simple_rnnlm.py
@@ -0,0 +1,52 @@
+import numpy as np
+import sys
+sys.path.append("../lib/layers")
+from time_affine import TimeAffine
+from time_embedding import TimeEmbedding
+from time_rnn import TimeRNN
+from time_softmax_with_loss import TimeSoftmaxWithLoss
+
+class SimpleRNNLM:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V  = vocab_size
+        D  = wordvec_size
+        H  = hidden_size
+        rn = np.random.randn
+        # Initialise weight
+        embed_W  = (rn(V, D) / 100).astype("f")
+        rnn_Wx   = (rn(D, H) / np.sqrt(D)).astype("f")
+        rnn_Wh   = (rn(H, H) / np.sqrt(H)).astype("f")
+        rnn_b    = np.zeros(H).astype("f")
+        affine_W = (rn(H, V) / np.sqrt(H)).astype("f")
+        affine_b = np.zeros(V).astype("f")
+        # Generate layers
+        self.layers = [
+            TimeEmbedding(embed_W),
+            TimeRNN(rnn_Wx, rnn_Wh, rnn_b, stateful=True),
+            TimeAffine(affine_W, affine_b)
+        ]
+        self.loss_layer = TimeSoftmaxWithLoss()
+        self.rnn_layer  = self.layers[1]
+        #Integrate all weight and gradients to a list each
+        self.params = []
+        self.grads  = []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads  += layer.grads
+
+    def forward(self, xs, ts):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        loss = self.loss_layer.forward(xs, ts)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        dout_list = []
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+            dout_list.append(dout)
+        return dout
+
+    def reset_state(self):
+        self.rnn_layer.reset_state()
---
+++ test/test_simple_rnnlm.py
@@ -0,0 +1,46 @@
+import unittest
+import numpy as np
+import sys
+sys.path.append("../lib/")
+sys.path.append("../lib/concerns")
+sys.path.append("../lib/layers")
+from simple_rnnlm import SimpleRNNLM
+from count_based_methods import CountBasedMethod
+
+class TestSimpleRNNLM(unittest.TestCase):
+    def setUp(self):
+        text = "You said good-bye and I said hello."
+        cbm = CountBasedMethod()
+        word_list = cbm.text_to_word_list(text)
+        word_to_id, *_ = cbm.preprocess(word_list)
+        vocab_size = len(word_to_id)
+        wordvec_size = 100
+        hidden_size  = 100
+        self.simple_rnnlm = SimpleRNNLM(vocab_size, wordvec_size, hidden_size)
+        self.xs = np.array([
+            [0, 4, 4, 1],
+            [4, 0, 2, 1]
+        ])
+        self.ts = np.array([
+            [0, 1, 0, 0],
+            [0, 0, 0, 1]
+        ])
+
+    def test_forward(self):
+        loss = self.simple_rnnlm.forward(self.xs, self.ts)
+        self.assertEqual(1.95, round(loss, 2))
+
+    def test_backward(self):
+        self.simple_rnnlm.forward(self.xs, self.ts)
+        dout = self.simple_rnnlm.backward()
+        self.assertEqual(None, dout)
+
+    def test_reset_state(self):
+        self.simple_rnnlm.forward(self.xs, self.ts)
+        self.simple_rnnlm.backward()
+        self.assertEqual((2, 100), self.simple_rnnlm.rnn_layer.h.shape)
+        self.simple_rnnlm.reset_state()
+        self.assertEqual(None, self.simple_rnnlm.rnn_layer.h)
+
+if __name__ == "__main__":
+    unittest.main()
