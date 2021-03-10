70067977e47a8585e0ad6788a65be0619cbbdeec
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-10 22:26:51 +0900

    Update train_seq2seq

--- train/train_seq2seq.py
+++ train/train_seq2seq.py
@@ -9,13 +9,16 @@ from sequence import Sequence
 from adam import Adam
 from eval_seq2seq import *
 from trainer import Trainer
-from seq2seq import Seq2Seq
 from peekly_seq2seq import PeekySeq2seq
 
 sequence = Sequence()
 (x_train, t_train), (x_test, t_test) = sequence.load_data("../texts/addition.txt")
-vocab = sequence.get_vocab()
-char_to_id, id_to_char = vocab
+char_to_id, id_to_char = sequence.get_vocab()
+
+# Reverse input?
+is_reverse = False  # True
+if is_reverse:
+    x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
 
 # Hyper parameters
 vocab_size   = len(char_to_id)
@@ -25,8 +28,8 @@ batch_size   = 128
 max_epoch    = 25
 max_grad     = 5.0
 
-# Generate a model. optimiser and trainer
-model     = Seq2Seq(vocab_size, wordvec_size, hidden_size)
+# Generate a model, optimiser and trainer
+model     = PeekySeq2seq(vocab_size, wordvec_size, hidden_size)
 optimiser = Adam()
 trainer   = Trainer(model, optimiser)
 
@@ -37,7 +40,7 @@ for epoch in range(max_epoch):
     for i in range(len(x_test)):
         question, correct = x_test[[i]], t_test[[i]]
         verbose = i < 10
-        correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose)
+        correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose, is_reverse)
     acc = float(correct_num) / len(x_test)
     acc_list.append(acc)
     print("Val acc %.3f%%" % (acc * 100))
