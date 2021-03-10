e41ebf634d3866db8bfe93555dc72a1be0f61792
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-10 22:17:08 +0900

    Update PeekySeq2seq

--- lib/peekly_seq2seq.py
+++ lib/peekly_seq2seq.py
@@ -1,10 +1,17 @@
+import sys
+sys.path.append("./layers")
 from seq2seq import Seq2Seq
+from encoder import Encoder
+from peeky_decoder import PeekyDecoder
+from time_softmax_with_loss import TimeSoftmaxWithLoss
 
 class PeekySeq2seq(Seq2Seq):
     def __init__(self, vocab_size, wordvec_size, hidden_size):
-        V, D, H = vocab_size, wordvec_size, hidden_size
+        V = vocab_size
+        D = wordvec_size
+        H = hidden_size
         self.encoder = Encoder(V, D, H)
         self.decoder = PeekyDecoder(V, D, H)
         self.softmax = TimeSoftmaxWithLoss()
-        self.params = self.encoder.params + self.decoder.params
-        self.grads = self.encoder.grads + self.decoder.grads
+        self.params  = self.encoder.params + self.decoder.params
+        self.grads   = self.encoder.grads  + self.decoder.grads
