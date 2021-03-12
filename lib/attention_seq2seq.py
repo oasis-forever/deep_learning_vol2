dafb4b9a89455f8c4d0df842e064d4c5c36c2033
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-12 23:35:59 +0900

    Commit AttentionSeq2Seq

---
+++ lib/attention_seq2seq.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+sys.path.append("./layers")
+from attention_encoder import AttentionEncoder
+from attention_decoder import AttentionDecoder
+from seq2seq import Seq2Seq
+
+class AttentionSeq2Seq(Seq2Seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V = vocab_size
+        D = wordvec_size
+        H = hidden_size
+        self.encoder = AttentionEncoder(*args)
+        self.decoder = AttentionDecoder(*args)
+        self.softmax = TimeSoftmaxWithLoss()
+        self.params = self.encoder.params + self.decoder.params
+        self.grads  = self.encoder.grads  + self.decoder.grads
