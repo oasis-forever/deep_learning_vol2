08a3e4c2c75e710738257ff6aafda6c3bc5986a0
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-13 00:49:58 +0900

    Commit train_attention_seq2seq

---
+++ train/train_atttention_seq2seq.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("../lib")
+sys.path.append("../lib/concerns")
+sys.path.append("../lib/layers")
+sys.path.append("../lib/models")
+sys.path.append("../lib/optimisers")
+from sequence import Sequence
+from adam import Adam
+from trainer import Trainer
+from eval_seq2seq import *
+from attention_seq2seq import AttentionSeq2Seq
+from peekly_seq2seq import PeekySeq2Seq
+
+sequence = Sequence()
+(x_train, t_train), (x_test, t_test) = sequence.load_data("../texts/date.txt")
+char_to_id, id_to_char = sequence.get_vocab()
+
+# Reverse input
+x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
+
+# Hyper parameters
+vocab_size   = len(char_to_id)
+wordvec_size = 16
+hidden_size  = 256 
+batch_size   = 128
+max_epoch    = 10
+max_grad     = 5.0
+
+# Generate a model, optimiser and trainer
+model     = AttentionSeq2Seq(vocab_size, wordvec_size, hidden_size)
+optimiser = Adam()
+trainer   = Trainer(model, optimiser)
+
+acc_list = []
+for epoch in range(max_epoch):
+    trainer.fit(x_train, t_train, max_epoch=1, batch_size=batch_size, max_grad=max_grad)
+    correct_num = 0
+    for i in range(len(x_test)):
+        question, correct = x_test[[i]], t_test[[i]]
+        verbose = i < 10
+        correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose, is_reverse=True)
+    acc = float(correct_num) / len(x_test)
+    acc_list.append(acc)
+    print("Val acc %.3f%%" % (acc * 100))
+
+model.save_params("../pkl/attention_seq2seq.pkl")
