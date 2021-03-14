7c8ed71a3fa6ef07c08d4f6f19863bc6652b9174
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-14 13:53:48 +0900

    Fix codes in train_attention_seq2seq

--- train/train_atttention_seq2seq.py
+++ train/train_atttention_seq2seq.py
@@ -11,19 +11,19 @@ from adam import Adam
 from trainer import Trainer
 from eval_seq2seq import *
 from attention_seq2seq import AttentionSeq2Seq
-from peekly_seq2seq import PeekySeq2Seq
 
 sequence = Sequence()
 (x_train, t_train), (x_test, t_test) = sequence.load_data("../texts/date.txt")
 char_to_id, id_to_char = sequence.get_vocab()
 
 # Reverse input
-x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
+x_train = x_train[:, ::-1]
+x_test  = x_test[:, ::-1]
 
 # Hyper parameters
 vocab_size   = len(char_to_id)
 wordvec_size = 16
-hidden_size  = 256 
+hidden_size  = 256
 batch_size   = 128
 max_epoch    = 10
 max_grad     = 5.0
@@ -38,11 +38,20 @@ for epoch in range(max_epoch):
     trainer.fit(x_train, t_train, max_epoch=1, batch_size=batch_size, max_grad=max_grad)
     correct_num = 0
     for i in range(len(x_test)):
-        question, correct = x_test[[i]], t_test[[i]]
-        verbose = i < 10
+        question = x_test[[i]]
+        correct  = t_test[[i]]
+        verbose  = i < 10
         correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose, is_reverse=True)
     acc = float(correct_num) / len(x_test)
     acc_list.append(acc)
     print("Val acc %.3f%%" % (acc * 100))
 
 model.save_params("../pkl/attention_seq2seq.pkl")
+
+plt.figure()
+x = np.arange(len(acc_list))
+plt.plot(x, acc_list, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.ylim(0, 1.0)
+plt.savefig("../img/train_seq2se2.png")
