f215517398bb9371fe1d17c7bad1e9748a59468a
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-14 13:49:27 +0900

    Fix codes in AttentionDecoder

--- lib/attention_decoder.py
+++ lib/attention_decoder.py
@@ -16,12 +16,12 @@ class AttentionDecoder:
         lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype("f")
         lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype("f")
         lstm_b  = np.zeros(4 * H).astype("f")
-        affine_W = (rn(H, V) / np.sqrt(H)).astype("f")
+        affine_W = (rn(2 * H, V) / np.sqrt(2 * H)).astype("f")
         affine_b = np.zeros(V).astype("f")
-        self.embed          = TimeEmbedding(embed_w)
-        self.lstm           = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
-        self.attention      = TimeAttention()
-        self.affine         = TimeAffine(affine_W, affine_b)
+        self.embed     = TimeEmbedding(embed_w)
+        self.lstm      = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        self.attention = TimeAttention()
+        self.affine    = TimeAffine(affine_W, affine_b)
         self.params = []
         self.grads  = []
         for layer in (self.embed, self.lstm, self.attention, self.affine):
@@ -42,7 +42,8 @@ class AttentionDecoder:
         dout = self.affine.backward(dscore)
         N, T, H2 = dout.shape
         H = H2 // 2
-        dc, ddec_hs0 = dout[:, : ,:H], dout[: ,: ,H:]
+        dc = dout[:, :, :H]
+        ddec_hs0 = dout[:, :, H:]
         denc_hs, ddec_hs1 = self.attention.backward(dc)
         ddec_hs = ddec_hs0 + ddec_hs1
         dout = self.lstm.backward(ddec_hs)
